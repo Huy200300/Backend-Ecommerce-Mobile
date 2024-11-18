83f7e83b4542a1cb7672b84a2884fbd099828e7a
Huy200300 <taihuy200300@gmail.com>
2024-11-18 22:13:44 +0700

    first update  backend

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ app.use(cookieParser());
 app.use(express.json());
 app.use("/api", router);
 
-app.get("/", (req, res) => {
+app.get("/test", (req, res) => {
   res.send("API is running!");
 });
 
