716fbce7c7b9c490d2c7dbd618accff25d75d6fc
Huy200300 <taihuy200300@gmail.com>
2024-11-18 21:27:13 +0700

    first commit user

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const connectDB = require("./configDB/db");
+const router = require("./router");
+const cookieParser = require("cookie-parser");
+const bodyParser = require("body-parser");
+
+const app = express();
+const PORT = process.env.PORT || 8080;
+connectDB();
+
+// app.use(cors());
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL,
+    credentials: true,
+  })
+);
+app.use(bodyParser.json());
+app.use(cookieParser());
+app.use(express.json());
+app.use("/api", router);
+
+app.listen(PORT, () => {
+  console.log(`server is running on port ` + PORT);
+});
