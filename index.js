e0f1ef92d8bf3c554ca927944f742c5682d21c3c
Huy200300 <taihuy200300@gmail.com>
2024-11-18 21:59:26 +0700

    first update  user backend

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const cookieParser = require("cookie-parser");
 const bodyParser = require("body-parser");
 
 const app = express();
-const PORT = process.env.PORT || 8080;
+// const PORT = process.env.PORT || 8080;
 connectDB();
 
 // app.use(cors());
@@ -21,10 +21,11 @@ app.use(bodyParser.json());
 app.use(cookieParser());
 app.use(express.json());
 app.use("/api", router);
+
 app.get("/", (req, res) => {
   res.send("API is running!");
 });
 
-app.listen(PORT, () => {
-  console.log(`server is running on port ` + PORT);
-});
+// app.listen(PORT, () => {
+//   console.log(`server is running on port ` + PORT);
+// });
