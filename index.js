ad4d0ad4c5a2f4451b07e56f77600b40dac4d0f1
Huy200300 <taihuy200300@gmail.com>
2024-11-18 21:44:41 +0700

    first update  user backend

--- index.js
+++ index.js
@@ -21,6 +21,9 @@ app.use(bodyParser.json());
 app.use(cookieParser());
 app.use(express.json());
 app.use("/api", router);
+app.get("/", (req, res) => {
+  res.send("API is running!");
+});
 
 app.listen(PORT, () => {
   console.log(`server is running on port ` + PORT);
