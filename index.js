fc95fccb9f1774e84161ceca724c2bc18c79ff3a
Huy200300 <taihuy200300@gmail.com>
2024-11-27 15:15:31 +0700

    update one

--- index.js
+++ index.js
@@ -18,6 +18,7 @@ app.use(
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use("/api", router);
 
